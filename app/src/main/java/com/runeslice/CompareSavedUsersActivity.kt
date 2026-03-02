9a6cff67f3f64409e5b77d7c35bf85fe6d8e0e46
mattlewer <mlewer@hotmail.co.uk>
2026-03-02 20:27:53 +0000

    Add cow boss, enable edge to edge

--- app/src/main/java/com/runeslice/CompareSavedUsersActivity.kt
+++ app/src/main/java/com/runeslice/CompareSavedUsersActivity.kt
@@ -2,6 +2,9 @@ package com.runeslice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.runeslice.databinding.ActivityCompareSavedUsersBinding
@@ -12,11 +15,18 @@ class CompareSavedUsersActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCompareSavedUsersBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+
         super.onCreate(savedInstanceState)
         binding = ActivityCompareSavedUsersBinding.inflate(layoutInflater)
         prepareNav()
         binding.returnToSearchBtn.setOnClickListener { onBackPressed() }
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            windowInsets
+        }
     }
 
 
--- app/src/main/java/com/runeslice/MainActivity.kt
+++ app/src/main/java/com/runeslice/MainActivity.kt
@@ -2,7 +2,10 @@ package com.runeslice
 
 import android.app.ProgressDialog
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.runeslice.databinding.ActivityMainBinding
@@ -13,16 +16,25 @@ const val BASE_URL = "https://secure.runescape.com/m=hiscore_oldschool/"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        try{ setSavedUsers() }
-        catch (e: Exception){}// No Saved Users
-        var navView = binding.bottomNavigationView
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            windowInsets
+        }
+
+        try { setSavedUsers() } catch (e: Exception) {}
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        navView.setupWithNavController(navController)
-        setContentView(binding.root)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
     override fun onStart() {
